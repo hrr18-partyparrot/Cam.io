03944142afc9af52231f144fa1ffaa7f8fe535ab
Ryan Busby <BusbyActual@gmail.com>
2016-09-01 22:11:26 -0700

    Added db and a few models to get things going.

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var PORT = process.env.PORT || 8080;
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/PartyParrot');
 var app = express();
 
 app.use(bodyParser.json());
