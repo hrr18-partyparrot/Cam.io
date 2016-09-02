f5fb6a364bfc030174d909c35e0f9a873cd69d64
Ryan Busby <BusbyActual@gmail.com>
2016-09-01 22:15:59 -0700

    Updated comments and added models to server.

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/PartyParrot');
+require('./mondels/models.js')
 var app = express();
 
 app.use(bodyParser.json());
