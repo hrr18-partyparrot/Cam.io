f112f2b5fc64c9be59440c045fe6e66fa2773a90
John Smalley <john.m.smalley@gmail.com>
2016-09-06 12:46:23 -0400

    Basics of stormpath in server.js and added express-strompath package

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,26 @@
 var express = require('express');
-var PORT = process.env.PORT || 8080;
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var middleware = './middleware/middleware';
+var stormpath = require('express-stormpath');
 
-
+var PORT = process.env.PORT || 8080;
 var db =  process.env.MONGOLAB_IVORY_URI || 'mongodb://localhost/PartyParrot';
+
 mongoose.connect(db);
 
 var app = express();
-
+require('./middleware/middleware')(app, express);
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
+app.get('/secrets',stormpath.loginRequired,function(req,res){
+  res.send('Hi ' + req.user.givenName);
+})
 
-app.get('/', function(req, res) {
-  res.send("Hellooooo World!")
+app.on('stormpath.ready', function() {
+  app.listen(PORT);
 });
 
-
-app.listen(PORT);
-
-<<<<<<< 636a7e8bb965bca4c20c74c04616e4dd8ab16ec5
-app.listen(PORT);
-=======
-
 module.exports = app;
->>>>>>> Seperated files and cleaned up server.js
