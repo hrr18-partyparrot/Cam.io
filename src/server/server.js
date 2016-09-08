75c10df8a327eb187e98840cffb225ba9b427ab3
John Smalley <john.m.smalley@gmail.com>
2016-09-08 14:39:19 -0400

    Able to save events to mongodb

--- src/server/server.js
+++ src/server/server.js
@@ -7,34 +7,52 @@ var stormpath = require('express-stormpath');
 var Eventbrite = require('eventbrite-node');
 var config = require('../config/eventbrite');
 var client = new Eventbrite(config.clientKey, config.clientSecret);
-
-
 var PORT = process.env.PORT || 8080;
 var db =  process.env.MONGODB_URI || 'mongodb://localhost/PartyParrot';
-
 mongoose.connect(db);
-
+//mongoose's promise library is depricated.
+mongoose.Promise = global.Promise;
 var app = express();
-
 app.use(stormpath.init(app, {
   application:{
     href: 'https://api.stormpath.com/v1/applications/38BYzfpt1mubNI49Sj9nC4'
   },
   website: true
 }));
-
 // require('./middleware/middleware')(app, express);
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
-
+var Event = require('./models/event');
+app.post('/create',stormpath.loginRequired, function(req,res){
+  var event = new Event({
+  name: req.body.event.name.text,
+  desc: req.body.event.description.text,
+  promoters: [req.user.fullName],
+  owner: req.user.username,
+  gPoint: req.body.gPoint,
+  gReward: req.body.gReward,
+  sPoint: req.body.sPoint,
+  sReward: req.body.gReward,
+  bPoint: req.body.bPoint,
+  bReward: req.body.gReward
+  })
+  event.save(function (err, post) {
+    if (err) {console.error(err)}
+    res.json(201,'Hey I posted ' + post);
+  });
+});
+app.get('/events', function (req, res, next) {
+  Event.find(function(err, events) {
+    if (err) { console.error(err) }
+    res.json(events);
+  })
+})
 app.get('/secrets', stormpath.loginRequired, function(req,res){
   res.send('Hi ' + req.user.givenName);
 })
-
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
-
 app.get('/authentication', function(req, res){
   var authUrl = client.getOAuthUrl();
   res.redirect(authUrl);
@@ -46,11 +64,7 @@ app.get('/authentication', function(req, res){
     console.log(response.access_token);
   });
 });
-
 app.on('stormpath.ready', function() {
   app.listen(PORT);
 });
-
-
-
 module.exports = app;
