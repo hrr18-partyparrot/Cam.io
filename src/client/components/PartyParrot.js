72e1832b8828849968f64907fe2b4e176d6dcdb7
Ryan Busby <BusbyActual@gmail.com>
2016-09-12 20:01:34 -0700

    disabling easter egg

--- src/client/components/PartyParrot.js
+++ src/client/components/PartyParrot.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import PartyParrot from '../../easterEgg/picture-fall.js';
+// import React from 'react';
+// import PartyParrot from '../../easterEgg/picture-fall.js';
 
 
-export default class UserDetails extends React.Component {
-  constructor() {
-    super();
-  }
-  render () {
-   return( <div>
-{$('div').css('visibility', 'visible')} {$("body").append('<img src="http://assets.marthastewart.com/styles/wmax-1500/d45/party-table-decoration-centerpiece-horiz-0824/party-table-decoration-centerpiece-horiz-0824_horiz.jpg?itok=wS9Mo61r.jpg" width ="100%" height="100%"></img> <audio autoplay="true" src="https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3">')}
-    </div>)
-  }
+// export default class UserDetails extends React.Component {
+//   constructor() {
+//     super();
+//   }
+//   render () {
+//   return( <div>
+// {$('div').css('visibility', 'visible')} {$("body").append('<img src="http://assets.marthastewart.com/styles/wmax-1500/d45/party-table-decoration-centerpiece-horiz-0824/party-table-decoration-centerpiece-horiz-0824_horiz.jpg?itok=wS9Mo61r.jpg" width ="100%" height="100%"></img> <audio autoplay="true" src="https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3">')}
+//     </div>)
+//   }
 
-}
+// }
 
-
+//This is to enable the easter egg. It's a very hacky solution as the images are always present and jquery just toggles to show them.
 
